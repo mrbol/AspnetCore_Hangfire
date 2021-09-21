d7b21aee2310ff427da3c0feddd1abc7a89cdeb5
tdr <tdr.evolution@gmail.com>
2021-09-21 00:01:57 -0300

    Hangfire

--- WebApplication1/Controllers/HangfireController.cs
+++ WebApplication1/Controllers/HangfireController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,58 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class HangfireController : ControllerBase
     {
+        [HttpPost]
+        [Route("welcome")]
+        public IActionResult Welcome(string userName)
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeMail(userName));
+            return Ok($"Job Id {jobId} Completed. Welcome Mail Sent!");
+        }
+
+        public void SendWelcomeMail(string userName)
+        {
+            //Logic to Mail the user
+            Console.WriteLine($"Welcome to our application, {userName}");
+        }
+        [HttpPost]
+        [Route("delayedWelcome")]
+        public IActionResult DelayedWelcome(string userName)
+        {
+            var jobId = BackgroundJob.Schedule(() => SendDelayedWelcomeMail(userName), TimeSpan.FromMinutes(2));
+            return Ok($"Job Id {jobId} Scheduled. Delayed Welcome Mail will be sent in 2 minutes!");
+        }
+
+        public void SendDelayedWelcomeMail(string userName)
+        {
+            //Logic to Mail the user
+            Console.WriteLine($"Welcome to our application, {userName}");
+        }
+        [HttpPost]
+        [Route("invoice")]
+        public IActionResult Invoice(string userName)
+        {
+            RecurringJob.AddOrUpdate(() => SendDelayedWelcomeMail(userName), Cron.Monthly);
+            return Ok($"Recurring Job Scheduled. Invoice will be mailed Monthly for {userName}!");
+        }
+
+        public void SendInvoiceMail(string userName)
+        {
+            //Logic to Mail the user
+            Console.WriteLine($"Here is your invoice, {userName}");
+        }
+        [HttpPost]
+        [Route("unsubscribe")]
+        public IActionResult Unsubscribe(string userName)
+        {
+            var jobId = BackgroundJob.Enqueue(() => UnsubscribeUser(userName));
+            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine($"Sent Confirmation Mail to {userName}"));
+            return Ok($"Unsubscribed");
+        }
+
+        public void UnsubscribeUser(string userName)
+        {
+            //Logic to Unsubscribe the user
+            Console.WriteLine($"Unsubscribed {userName}");
+        }
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -26,7 +26,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHangfire(x => x.UseSqlServerStorage("Server=(localdb)\\mssqllocaldb;Database=onionDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            services.AddHangfire(x => x.UseSqlServerStorage("Server=(localdb)\\mssqllocaldb;Database=hangfireDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
             services.AddHangfireServer();
             services.AddControllers();
         }
